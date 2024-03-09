4026c5c18d7e6b5f126aa70da7bf3b38589281aa
Aniket <73299058+Aniket-x-code@users.noreply.github.com>
2024-03-09 14:37:57 +0530

    reduce fn working demo

--- map.js
+++ map.js
@@ -17,3 +17,11 @@ const ouput = radius.map( double);
 const bin = radius.map(binary);
 console.log(ouput);
 console.log(bin);
+
+// reduce fn ( fn (acc, cur), initial value of acc)
+
+const out = radius.reduce((acc, cur) =>{
+     acc= acc+cur;
+     return acc;
+},0);
+console.log(out); // here 0 is teh initial value of acc because sum ko 0 se initial karna hai
