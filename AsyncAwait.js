2a5dc8edfd9999d52652f1b90fcd4d853e0c22ae
Aniket <73299058+Aniket-x-code@users.noreply.github.com>
2024-03-15 21:54:39 +0530

    Add files via upload

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -2,15 +2,22 @@ console.log( "Async linked");
 
 
 const p1 = new Promise((resolve, reject) => {
-    resolve("p1 resolved");
+  setTimeout( ()=>{
+    resolve ("p1 resolved");
+},2000)
+   
 }
 )
 
  async function getdata()
 {
    const val = await p1;
+   console.log("Namaste js");
+   console.log(val);
 
-  // using async and await 
+  // using async and await - await can only be used inside async function only
 }
 
 getdata();
+
+
