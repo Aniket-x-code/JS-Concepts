46a44026346c7d9d2d01c72261d95b702bfc7b03
Aniket <73299058+Aniket-x-code@users.noreply.github.com>
2024-03-17 21:57:13 +0530

    2 parallel promise resolving

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -4,16 +4,29 @@ console.log( "Async linked");
 const p1 = new Promise((resolve, reject) => {
   setTimeout( ()=>{
     resolve ("p1 resolved");
-},2000)
-   
+},1000)
 }
 )
 
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout( ()=>{
+    resolve ("p2 resolved");
+},5000)
+}
+)
+
+
  async function getdata()
 {
+  console.log("Hello js");
    const val = await p1;
    console.log("Namaste js");
    console.log(val);
+
+    const val2 = await p2;
+    console.log(val2);
+    console.log("End");
 
   // using async and await - await can only be used inside async function only
 }
